6044a5e7ccb099bf44114d4959a8dba496b4a2ef
JosephYanez27 <Chocheyg27@gmail.com>
2026-04-11 10:39:35 -0600

    Delete js directory

--- js/models/producto.model.js
+++
@@ -1,7 +0,0 @@
-export class Producto {
-    constructor(id, nombre,descripcion) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion= descripcion;
-    }
-}
--- js/services/menu.service.js
+++
@@ -1,6 +0,0 @@
-import { API_URL } from "../config/api.config.js";
-
-export async function obtenerMenu() {
-    const response = await fetch(`${API_URL}menu/completo`);
-    return await response.json();
-}
